1794f204bcc8fa2b162f1fea948f4fc9ff7355c2
Pranto Protim Baidya <pranto.protim.cse@ulab.edu.bd>
2026-09-19 01:08:41 +0600

    Add files via upload
    fronted code file

---
+++ layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"SmartRent | Property Rental",description:"Smart property rental platform"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
